14e8a0676b71cf8d863a5b8696cee6c3a529da5f
Viktoria Kravchenko <taiyibe@yandex.ru>
2023-03-11 16:21:38 +0300

    Оптимизация MagicBox.java

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -12,35 +12,18 @@ public class MagicBox<T> {
     }
 
     public boolean add(T item) {
-        boolean is_empty = false;
         for (int i = box.length - 1; i >= 0; i--) {
             if (box[i] == null) {
-                is_empty = true;
-            } else {
-                break;
+                box[count] = item;
+                count++;
+                return true;
             }
         }
-
-        if (is_empty) {
-            box[count] = item;
-            count++;
-            return true;
-        } else {
-            return false;
-        }
+        return false;
     }
 
     public T pick() {
-        boolean is_empty = false;
-        for (int i = box.length - 1; i >= 0; i--) {
-            if (box[i] == null) {
-                is_empty = true;
-            } else {
-                break;
-            }
-        }
-
-        if (is_empty) {
+        if (box.length != count) {
             throw new RuntimeException("Коробка не полна, ячеек осталось: " + Integer.toString(box.length - count));
         } else {
             return box[rnd.nextInt(box.length)];
